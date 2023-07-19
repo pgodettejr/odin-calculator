a3f194652c9650c63f252fc911f2302d1f6219b6
pgodettejr <paul.godette@gmail.com>
2023-07-19 12:37:03 -0400

    Revert JS changes that incorrectly showed eval()

--- calc.js
+++ calc.js
@@ -1,10 +1,7 @@
 const container = document.getElementById("container");
 const output = document.getElementById("output");
-// const data = document.getElementById("calc_display");
-const numButtons = document.querySelectorAll(".number");
-const opButtons = document.querySelectorAll(".operator");
-const equalButton = document.querySelector(".equals");
-const clearButton = document.querySelector(".clear");
+const data = document.getElementById("calc_display");
+const buttons = document.querySelectorAll(".buttons button");
 
 // Parts of a calculator operation (Me)
 let first = 0;
@@ -20,24 +17,6 @@ const add = function(a, b) {
 const subtract = function(c, d) {
   return c - d;
 };
-
-/* const sum = function(multi) {
-  let sum = 0;
-  
-  if(multi.length === 0) {
-    return sum;
-  }
-
-  for (let i = 0; i < multi.length; i++) {
-    if(isNaN(multi[i])) {
-      return "ERROR";
-    }
-
-    sum += multi[i];
-  }
-
-  return sum;
-}; */
 
 const multiply = function(array) {
   return array.length
@@ -66,17 +45,12 @@ function operate(operator, first, second) {
   }
 };
 
-// Clears display (Google - CodePen)
-function clearDisplay() {
-  output.value = '';
-}
-
 // Stores first number (StackOverflow)
 function storeFirst() {
   first = parseFloat(output.value);
   output.value = '';
-  // operator = '';
-  // return first;
+  operator = '';
+  return first;
 }
 
 // Function that stores the operator? StackOverflow code doesn't quite make sense here
@@ -85,18 +59,96 @@ function storeFirst() {
 function storeSecond() {
   second = parseFloat(output.value);
   output.value = operate(operator, first, second);
-  first = parseFloat(output.value);
-  // return second;
+  first = parseFloat(output.value); // Added later. Not part of OG code
+  return second;
 }
 
 // Defines when first number & operator are set then calls second number (StackOverflow)
 function answer() {
-  clearDisplay();
   if (first && operator) {
     storeSecond();
-    // return operate(operator, first, second);
   }
 }
+
+// Runs operate, then populates answer on display as buttons are clicked
+buttons.forEach((button) => {
+  button.addEventListener('click', function() {
+    if (button.dataset.type === 'operator') {
+      operator = button.value;
+      answer();
+    } else if (button.dataset.type === 'operand') {
+      output.value += button.value;
+    } else if (button.dataset.type === 'clear') {
+      output.value = '';
+      first = 0;
+      second = 0;
+      operator = '';
+    }
+  })
+})
+
+/* This SHOULD add event listeners for operator buttons but it doesn't (ChatGPT)
+// This code seems to not make sense given the code above is nearly the same thing but for all buttons
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', function() {
+    operator = button.value;
+  });
+}); */
+
+
+// Old and/or incorrect code
+
+/* const numButtons = document.querySelectorAll(".number");
+const opButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+
+const sum = function(multi) {
+  let sum = 0;
+  
+  if(multi.length === 0) {
+    return sum;
+  }
+
+  for (let i = 0; i < multi.length; i++) {
+    if(isNaN(multi[i])) {
+      return "ERROR";
+    }
+
+    sum += multi[i];
+  }
+
+  return sum;
+};
+
+const power = function(x, y) {
+  return Math.pow(x, y)
+};
+
+const factorial = function(num) {
+  let result = num;
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+
+  return result;
+}; 
+
+// Clears display (Google - CodePen)
+function clearDisplay() {
+  output.value = '';
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', operate);
+  answer();
+});
 
 // Operator button functionality; populates next number on display (ChatGPT)
 opButtons.forEach((button) => {
@@ -136,39 +188,4 @@ clearButton.addEventListener('click', () => {
     second = 0;
     operator = '';
   }
-});
-
-/* This SHOULD add event listeners for operator buttons but it doesn't (ChatGPT)
-// This code seems to not make sense given the code above is nearly the same thing but for all buttons
-operatorButtons.forEach((button) => {
-  button.addEventListener('click', function() {
-    operator = button.value;
-  });
-}); */
-
-
-// Old and/or incorrect code
-
-/* const power = function(x, y) {
-  return Math.pow(x, y)
-};
-
-const factorial = function(num) {
-  let result = num;
-
-  if (num === 0 || num === 1) {
-    return 1;
-  }
-
-  while (num > 1) {
-    num--;
-    result *= num;
-  }
-
-  return result;
-}; 
-
-buttons.forEach((button) => {
-  button.addEventListener('click', operate);
-  answer();
 }); */
